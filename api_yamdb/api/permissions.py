e5003ce7cc8b7ab2a9a76ed987a0cfb063549928
Denis Maltsev <mdu@arenadata.io>
2022-08-10 22:42:19 +0300

    Added admin for Category,Title,Genre. Change permissions. Fixed problems after testing

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -21,5 +21,5 @@ class AdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated and request.user.is_staff)
+            or (request.user.is_authenticated and request.user.role == 'admin')
         )
